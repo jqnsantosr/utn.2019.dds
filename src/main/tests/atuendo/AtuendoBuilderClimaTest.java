f5afa5bc71798511e3cb91ddb66b5c1a07c7649c
Fiorella <fiorella.fontau@gmail.com>
2019-06-20 19:20:18 -0300

    Test Clima
    Test Clima con Fer

--- src/main/tests/atuendo/AtuendoBuilderClimaTest.java
+++ src/main/tests/atuendo/AtuendoBuilderClimaTest.java
@@ -36,7 +36,7 @@ class AtuendoBuilderClimaTest {
     @Test
     void crearNuevoAtuendoParaClimaFrio()
     {
-        Guardarropa unGuardarropa = new Guardarropa("Test");
+        Guardarropa unGuardarropa = new Guardarropa("Test Clima Frio");
 
         unGuardarropa.setClimaController(climaController);
         when(climaController.getClima()).thenReturn(clima);
@@ -51,7 +51,7 @@ class AtuendoBuilderClimaTest {
         TipoPrenda buzo = new TipoPrenda("Buzo");
         buzo.setCalor(10);
         buzo.agregarParteCuerpo(PartesCuerpo.TORSO);
-        Prenda buzoVerde = new Prenda("Buzo Verde", remera, "Verde");
+        Prenda buzoVerde = new Prenda("Buzo Verde", buzo, "Verde");
 
         TipoPrenda pantalon = new TipoPrenda("Pantalon");
         pantalon.setCalor(15);
@@ -91,20 +91,120 @@ class AtuendoBuilderClimaTest {
 
         assertTrue(unAtuendo.tiene_prenda(buzoVerde));
         assertTrue(unAtuendo.tiene_prenda(pantalonSnow));
-        assertTrue(unAtuendo.tiene_prenda(nikesAmarillas));
         assertTrue(unAtuendo.tiene_prenda(gorraBlanca));
+        assertTrue(unAtuendo.tiene_prenda(nikesAmarillas));
     }
 
     @Test
     void crearNuevoAtuendoParaClimaCaliente()
     {
+        Guardarropa unGuardarropa = new Guardarropa("Test Clima Caliente");
 
+        unGuardarropa.setClimaController(climaController);
+        when(climaController.getClima()).thenReturn(clima);
+        when(clima.getClimateNow()).thenReturn(currentClima);
+        when(currentClima.getTemperature()).thenReturn(35.0);
+
+        TipoPrenda remera = new TipoPrenda("Remera");
+        remera.setCalor(5);
+        remera.agregarParteCuerpo(PartesCuerpo.TORSO);
+        Prenda remeraVerde = new Prenda("Remera Verde", remera, "Verde");
+
+        TipoPrenda pantalon = new TipoPrenda("Pantalon");
+        pantalon.setCalor(10);
+        pantalon.agregarParteCuerpo(PartesCuerpo.PIERNAS);
+        Prenda jeanGris = new Prenda("Jean Gris", pantalon, "Gris");
+
+        TipoPrenda gorra = new TipoPrenda("Gorra");
+        gorra.setCalor(3);
+        gorra.agregarParteCuerpo(PartesCuerpo.CABEZA);
+        Prenda gorraNegra = new Prenda("Gorra negra", gorra, "Negra");
+
+        TipoPrenda zapatilla = new TipoPrenda("Zapatilla");
+        zapatilla.setCalor(2);
+        zapatilla.agregarParteCuerpo(PartesCuerpo.CALZADO);
+        Prenda nikesVerdes = new Prenda("Nikes Verdes", zapatilla, "Verdes");
+
+        unGuardarropa.addPrenda(remeraVerde);
+        unGuardarropa.addPrenda(jeanGris);
+        unGuardarropa.addPrenda(gorraNegra);
+        unGuardarropa.addPrenda(nikesVerdes);
+
+        List<PartesCuerpo> partesPedidas = new ArrayList<>();
+        partesPedidas.add(PartesCuerpo.TORSO);
+        partesPedidas.add(PartesCuerpo.PIERNAS);
+        partesPedidas.add(PartesCuerpo.CABEZA);
+        partesPedidas.add(PartesCuerpo.CALZADO);
+
+        Atuendo unAtuendo = unGuardarropa.crearAtuendoClima(partesPedidas);
+        unAtuendo.mostrarAtuendo();
+
+        assertTrue(unAtuendo.tiene_prenda(remeraVerde));
+        assertTrue(unAtuendo.tiene_prenda(jeanGris));
+        assertTrue(unAtuendo.tiene_prenda(gorraNegra));
+        assertTrue(unAtuendo.tiene_prenda(nikesVerdes));
     }
 
     @Test
     void crearNuevoAtuendoParaClimaFrioTieneMasDeUnaCapa()
     {
+        Guardarropa unGuardarropa = new Guardarropa("Test Clima Frio Power");
 
+        unGuardarropa.setClimaController(climaController);
+        when(climaController.getClima()).thenReturn(clima);
+        when(clima.getClimateNow()).thenReturn(currentClima);
+        when(currentClima.getTemperature()).thenReturn(15.0);
+
+        TipoPrenda remera = new TipoPrenda("Remera");
+        remera.setCalor(15);
+        remera.agregarParteCuerpo(PartesCuerpo.TORSO);
+        Prenda remeraTermicaBlanca = new Prenda("Remera Termica Blanca", remera, "Blanca");
+
+        TipoPrenda buzo = new TipoPrenda("Buzo");
+        buzo.setCalor(15);
+        buzo.agregarParteCuerpo(PartesCuerpo.TORSO);
+        Prenda buzoNegro = new Prenda("Buzo Negro", buzo, "Negro");
+
+        TipoPrenda campera = new TipoPrenda("Campera");
+        campera.setCalor(20);
+        campera.agregarParteCuerpo(PartesCuerpo.TORSO);
+        Prenda camperaGris = new Prenda("Campera Gris", campera, "Gris");
+
+        TipoPrenda pantalon = new TipoPrenda("Pantalon");
+        pantalon.setCalor(15);
+        pantalon.agregarParteCuerpo(PartesCuerpo.PIERNAS);
+        Prenda jeanNegro = new Prenda("Jean Negro", pantalon, "Negro");
+
+        TipoPrenda gorra = new TipoPrenda("Gorra");
+        gorra.setCalor(3);
+        gorra.agregarParteCuerpo(PartesCuerpo.CABEZA);
+        Prenda gorraLanaNegra = new Prenda("Gorra de lana negra", gorra, "Negra");
+
+        TipoPrenda zapatilla = new TipoPrenda("Zapatilla");
+        zapatilla.setCalor(2);
+        zapatilla.agregarParteCuerpo(PartesCuerpo.CALZADO);
+        Prenda adidasNegra = new Prenda("Adidas Negras", zapatilla, "Negras");
+
+        unGuardarropa.addPrenda(remeraTermicaBlanca);
+        unGuardarropa.addPrenda(buzoNegro);
+        unGuardarropa.addPrenda(camperaGris);
+        unGuardarropa.addPrenda(jeanNegro);
+        unGuardarropa.addPrenda(gorraLanaNegra);
+        unGuardarropa.addPrenda(adidasNegra);
+
+        List<PartesCuerpo> partesPedidas = new ArrayList<>();
+        partesPedidas.add(PartesCuerpo.TORSO);
+        partesPedidas.add(PartesCuerpo.PIERNAS);
+        partesPedidas.add(PartesCuerpo.CABEZA);
+        partesPedidas.add(PartesCuerpo.CALZADO);
+
+        Atuendo unAtuendo = unGuardarropa.crearAtuendoClima(partesPedidas);
+        unAtuendo.mostrarAtuendo();
+
+        assertTrue(unAtuendo.tiene_prenda(camperaGris));
+        assertTrue(unAtuendo.tiene_prenda(jeanNegro));
+        assertTrue(unAtuendo.tiene_prenda(jeanNegro));
+        assertTrue(unAtuendo.tiene_prenda(adidasNegra));
     }
 
     @Test
